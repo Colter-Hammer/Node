dda4dfed1347952045a517d848b61e3cd37ab493
Colter-Hammer <therondowdle7@gmail.com>
2018-02-09 18:33:26 -0700

    async trials. This stuff is tough

--- 9_jugglingAsync.js
+++ 9_jugglingAsync.js
@@ -11,18 +11,78 @@
  * Got lucky
  ******************* */
 
- const http = require('http');
+//  const http = require('http');
 
-for (let i = 2; i < process.argv.length; i++) {
-    http.get(process.argv[i], (data) => {
+// for (var i = 2; i < process.argv.length; i++) {
+//     http.get(process.argv[i], (data) => {
+//         data.setEncoding('utf8');
+//         let str = '';
+//         data.on('data', data => str += data);
+//         data.on('error', error => console.log(error));
+//         data.on('end', data => console.log(str));
+//     });
+// }
+
+const http = require('http');
+var list = process.argv;
+var array = [];
+var count = 0;
+var str = '';
+
+function printResult() {
+    for (var i = 0; i < list.length - 2; i++) {
+        console.log(array[i]);
+    }
+}
+for (var i = 0; i < list.length - 2; i++) {
+    http.get(list[2 + i], data => {
         data.setEncoding('utf8');
-        let str = '';
         data.on('data', data => str += data);
-        data.on('error', error => console.log(error));
-        data.on('end', data => console.log(str));
+        data.on('error', error => {
+            console.log(error);
+        });
+        data.on('end', (data) => {
+            array[i] = str;
+            // console.log(str);
+            // console.log(array);
+            count++;
+            if (count == 3) {
+                printResult();
+                // console.log(array);
+            }
+        });
     });
 }
 
+// for (var i = 0; i < list.length - 2; i++) {
+//     httpGet(i);
+// }
+
+// //this works
+// function httpGet(index) {
+//     http.get(list[2 + index], data => {
+//         var str = '';
+//         data.setEncoding('utf8');
+//         data.on('data', data => str += data);
+//         data.on('error', error => {
+//             console.log(error);
+//         });
+//         data.on('end', (data) => {
+//             array[index] = str;
+//             // console.log(str);
+//             // console.log(array);
+//             count++;
+//             if (count == 3) {
+//                 printResult();
+//                 // console.log(array);
+//             }
+//         });
+//     });
+// }
+
+// for (var i = 0; i < list.length - 2; i++) {
+//     httpGet(i);
+// }
 
 
 
@@ -30,44 +90,44 @@ for (let i = 2; i < process.argv.length; i++) {
  * Their answer
  *********************** */
 
-var http = require('http')
-var bl = require('bl')
-var results = []
-var count = 0
+// var http = require('http')
+// var bl = require('bl')
+// var results = []
+// var count = 0
 
-// Prints all results
-function printResults() {
-    for (var i = 0; i < 3; i++) {
-        console.log(results[i])
-    }
-}
+// // Prints all results
+// function printResults() {
+//     for (var i = 0; i < 3; i++) {
+//         console.log(results[i])
+//     }
+// }
 
-/***************************
- * Main function where the logic takes place
- *************************** */
-function httpGet(index) {
-    http.get(process.argv[2 + index], function (response) {
+// /***************************
+//  * Main function where the logic takes place
+//  *************************** */
+// function httpGet(index) {
+//     http.get(process.argv[2 + index], function (response) {
 
-        // I only don't understand .pipe and bl();
-        response.pipe(bl(function (err, data) {
-            if (err) {
-                return console.error(err)
-            }
+//         // I only don't understand .pipe and bl();
+//         response.pipe(bl(function (err, data) {
+//             if (err) {
+//                 return console.error(err)
+//             }
 
-            // Converts to string and increases count
-            results[index] = data.toString()
-            count++
-             
-            // Once all three of the lines are done processing, call printResults()
-            if (count === 3) {
-                printResults()
-            }
-        }))
-    })
-}
+//             // Converts to string and increases count
+//             results[index] = data.toString()
+//             count++
 
-// Calls the function where logic is.
-// Looped so that it does it with all three of the files given
-for (var i = 0; i < 3; i++) {
-    httpGet(i)
-}
+//             // Once all three of the lines are done processing, call printResults()
+//             if (count === 3) {
+//                 printResults()
+//             }
+//         }))
+//     })
+// }
+
+// // Calls the function where logic is.
+// // Looped so that it does it with all three of the files given
+// for (var i = 0; i < 3; i++) {
+//     httpGet(i)
+// }
